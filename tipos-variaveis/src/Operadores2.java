74653d8aa2c1693ea0a5714161dabedb468b818e
htaDouglas <htadouglas@gmail.com>
2024-06-11 16:52:29 -0300

    Merge branch 'main' of https://github.com/htaDouglas/dio-trilha-git-java-basico

---
+++ tipos-variaveis/src/Operadores2.java
@@ -0,0 +1,22 @@
+public class Operadores2 {
+    public static void main(String[] args) {
+        
+        int numero =5;
+        //x repeticao
+ 
+        //numero=numero+1;
+ 
+        System.out.println( numero -- );
+ 
+        System.out.println(numero);
+ 
+ 
+ 
+ 
+ 
+        boolean variavel = true;
+ 
+        variavel = !variavel;
+        System.out.println(variavel);
+    }
+}
